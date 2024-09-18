441ddfa9b3a6e3bbc57a25d6d8cc27a76346b730
mlaskowski7 <mtlaskowski7@gmail.com>
2024-09-18 16:13:56 +0200

    defined routes for other dashboards view

--- src/components/DashboardNavbar.svelte
+++ src/components/DashboardNavbar.svelte
@@ -16,7 +16,7 @@
     <NavbarItem label="Profile" link="/you" icon={faUser}/>
     <NavbarItem label="Friends" link="/friends" icon={faUsers}/>
     <NavbarItem label="Dashboard" link="/dashboard" icon={faTableCells}/>
-    <NavbarItem label="Leaderboard" link="/dashboard" icon={faRankingStar}/>
+    <NavbarItem label="Leaderboard" link="/leaderboard" icon={faRankingStar}/>
     <button on:click={logout} class="uppercase font-bold p-5 rounded-xl flex flex-row gap-2 items-center justify-center font-mono bg-accent hover:brightness-75 ease-in-out duration-300">
         <FontAwesomeIcon icon={faRightFromBracket}/>
         logout
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+	import { icon } from "@fortawesome/fontawesome-svg-core";
+	import { faRobot } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/svelte-fontawesome";
+
 
 </script>
 
-<div class="w-full h-[5vh] rounded-b-lg flex justify-center items-center px-10 py-5">
-    <a href="/" class="mt-4 uppercase font-roboto p-3 text-black bg-accent rounded-2xl font-extrabold text-[16px] tracking-widest">
-        Titans
+<div class="w-full h-[5vh] rounded-b-lg flex justify-end items-center px-5 py-5">
+    <a href="/" class="mt-4 uppercase font-roboto p-3 text-accent rounded-2xl font-extrabold text-[16px] tracking-widest">
+        <FontAwesomeIcon icon={faRobot} /> TITANS
     </a>
 </div>
