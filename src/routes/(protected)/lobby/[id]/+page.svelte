fe4629360bd607a95b41a9e49d43391cb1ac63f5
mlaskowski7 <mtlaskowski7@gmail.com>
2024-09-30 19:49:19 +0200

    lobby page

--- src/routes/(protected)/lobby/[id]/+page.svelte
+++ src/routes/(protected)/lobby/[id]/+page.svelte
@@ -27,13 +27,30 @@
 	})
 </script>
 
-<div class="flex flex-col w-full gap-40 ml-20 p-10 justify-center items-center">
-	<h1 class="tracking-wider text-accent p-4 text-[32px] uppercase">{currentLobby?.name}</h1>
-	<div class="flex flex-row gap-2">
-		{#each usersInLobby as current}
-			<div>
-				{current?.username} ({current?.points})
-			</div>
-		{/each}
+<div class="flex flex-row gap-20 w-full items-center justify-between mx-40">
+	<div class="flex flex-col gap-10 ml-20 p-10 justify-center items-center">
+		<h1 class="tracking-wider text-accent font-extrabold p-4 text-[32px] uppercase">{currentLobby?.name}</h1>
+		<div class="flex flex-row gap-2">
+			<div>Max players: {currentLobby?.max_players}</div>
+		</div>
+		<div class="flex flex-row gap-2">
+			{#each usersInLobby as current}
+				<div>
+					{current?.username} ({current?.points})
+				</div>
+			{/each}
+		</div>
+		<div class="flex flex-row gap-4">
+			<div class="px-4 py-2 rounded-xl bg-secondaryBg uppercase font-mono">{currentLobby?.state}</div>
+			<button class="bg-accent px-2 py-1 text-[12px] rounded-xl uppercase hover:brightness-75 duration-300 ease-in-out">Next state</button>
+		</div>
+	</div>
+	<div class="flex flex-col gap-2 justify-center items-center">
+		<h2 class="text-accent font-extrabold text-[32px]">Lobby States</h2>
+		<div class="px-4 py-2 rounded-xl bg-secondaryBg font-mono">1. WAITING -> just created</div>
+		<div class="px-4 py-2 rounded-xl bg-secondaryBg font-mono">2. CONFIGURING -> players joining</div>
+		<div class="px-4 py-2 rounded-xl bg-secondaryBg font-mono">3. ONGOING -> game in progress</div>
+		<div class="px-4 py-2 rounded-xl bg-secondaryBg font-mono">4. FINISHED -> game finished</div>
 	</div>
 </div>
+
