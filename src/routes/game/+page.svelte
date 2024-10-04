b87a626d4b674edd1ec44d94e66c830eca72f259
mlaskowski7 <mtlaskowski7@gmail.com>
2024-10-04 13:03:17 +0200

    game grid

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -2,7 +2,11 @@
 
 </script>
 
-<div class="w-[100vw] h-[90vh]">
 
-
+<div class="w-[100vw] h-[90vh] grid grid-cols-12">
+	{#each Array(12 * 12	) as _, i}
+		<div class="border-2 border-white flex items-center justify-center">
+			{i}
+		</div>
+	{/each}
 </div>
